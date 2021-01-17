5905a0ed2cd4a5e744c9de8b41a08da8ecabf80b
jtmst <josh.troy.mills@gmail.com>
2021-01-17 13:15:55 -0500

    New post routes and form

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,13 @@
 const Post = require('../models/post');
 
 module.exports = {
+    // Post Index
     async getPosts(req, res, next) {
         let posts = await Post.find({})
         res.render('posts/index', { posts })
+    },
+    // Post New
+    async newPost(req, res, next) {
+        res.render('posts/new');
     }
 }
